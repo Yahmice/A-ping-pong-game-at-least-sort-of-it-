d55d45af8c05a354cfd9638458ad335078cb7fb9
Yahmice <83534639+Yahmice@users.noreply.github.com>
2021-05-16 15:22:43 +0500

    Add files via upload

---
+++ A ping pong game/Ping-ping.py
@@ -0,0 +1,65 @@
+from pygame import *
+
+#классы 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, size_x, size_y,player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))\
+
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y = -self.speed
+        if keys[K_s] and self.rect.y <= 5:
+            self.rect.y = +self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y = -self.speed
+
+#окно приложения
+window = display.set_mode((700,500))
+display.set_caption('Ping-Pong')
+
+#фон
+background = transform.scale(image.load('bg.png'), (700,500))
+
+#спрайты
+player_l = Player('paddle.png', 65, 65, 100, 430, 4)
+player_r = Player('paddle.png',65, 65, 100, 430, 4) 
+
+
+
+
+
+
+
+
+
+FPS = 75
+running = True
+finish = False
+
+
+while running:
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+    if finish != True:
+        window.blit(background, (0,0))
+        player_l.update_l()
+        player_r.update_r
+
+        player_l.reset()
+        player_r.reset()
+        
+
+        
